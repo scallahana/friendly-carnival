8d2c9ff38c2b4efe312dd72d4e43bbc06bdd9d9a
Anthony <maplemusic1@icloud.com>
2016-12-10 14:15:19 -0700

    Initialized repo

---
+++ db/migrate/20161210002024_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+	def change
+	 add_index :users, :email, unique: true
+    create_table :users do |t|
+		 
+      t.string :email
+      t.string :crypted_password
+      t.string :password_salt
+      t.string :persistence_token
+      t.timestamps
+    end
+  end
+end
